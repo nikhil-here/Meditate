4dbb11b0465eab163478f890f880d46ac70db9a9
nikhil-here <nikhilmandlik43@gmail.com>
2021-09-10 18:00:45 +0530

    Initial commit

---
+++ app/src/main/java/com/nikhilhere/meditate/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.nikhilhere.meditate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.nikhilhere.meditate.ui.HomeScreen
+import com.nikhilhere.meditate.ui.theme.MeditateTheme
+
+class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MeditateTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
+
